67c71dd12589b7705a4a79e784888be514859270
Hüseyin Deniz KIVRAK <huseyindenizkivrak@gmail.com>
2023-04-22 15:08:32 +0300

    Remove storybook-addon-react-router-v6 (#19)

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,6 +1,6 @@
 const config = {
   "stories": ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
-  "addons": ["@storybook/addon-links", "@storybook/addon-essentials", "@storybook/addon-interactions", "@storybook/preset-create-react-app", "storybook-addon-react-router-v6", "@chakra-ui/storybook-addon",],
+  "addons": ["@storybook/addon-links", "@storybook/addon-essentials", "@storybook/addon-interactions", "@storybook/preset-create-react-app", "@chakra-ui/storybook-addon",],
   "framework": {
     name: "@storybook/react-webpack5",
     options: {}
